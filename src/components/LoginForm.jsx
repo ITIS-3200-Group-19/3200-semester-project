21c6a5cb10664d825c800d853d73296a525dbac4
Benjamin LaCroix <ben.lac5@icloud.com>
2026-04-16 20:01:29 -0400

    Commented out the code in LoginForm that needs to be abstracted out between here and the backend.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,11 +4,12 @@
 import { useState } from "react";
 
 // Node.js Crypto Module for hashing and salting passwords
-const { scrypt, randomBytes } = require("node:crypto");
+// const { scrypt, randomBytes } = require("node:crypto");
 
 function LoginForm({ setIsLoggedIn, mode }) {
   // State to track if an error message should be shown (e.g., if login fails).
-  const [showError, setShowError] = useState(false);
+  const [showError, setShowError] = useState(false); // Tracking if error in finding login details.
+  const [showHashError, setShowHashError] = useState(false); // Tracking if error occured during hashing.
   const [enteredUsername, setEnteredUsername] = useState(""); // State to track the username input
   const [enteredPassword, setEnteredPassword] = useState(""); // State to track the password input
 
@@ -17,67 +18,77 @@ function LoginForm({ setIsLoggedIn, mode }) {
     // Handle login logic here (e.g., send credentials to the server to check if they are valid,).
     // Set logged-in state if successful, or show an error message if not.
 
+    // //testing purposes:
+    setIsLoggedIn(true); // TODO: Remove this line after implementing the actual login logic.
+
     // Check database to see if the username exists, if not show error.
     // Username checking logic will differ based on the security level (mode).
-    const usernameValid = false;
-    if (mode === "Vulnerable") {
-      // TODO: Unsecurely check if the username exists in the database.
-    } else {
-      // mode is "Secure":
-      // TODO: Securely check if the username exists in the database.
-    }
+    // const usernameValid = false;
+    // if (mode === "Vulnerable") {
+    //   // TODO: Unsecurely check if the username exists in the database.
+    // } else {
+    //   // mode is "Secure":
+    //   // TODO: Securely check if the username exists in the database.
+    // }
 
-    if (usernameValid) {
-      verifyPassword(); // If the username is valid, verify the password.
-    } else {
-      setShowError(true); // If the username is not valid, show the error message.
-    }
+    // if (usernameValid) {
+    //   verifyPassword(); // If the username is valid, verify the password.
+    //   setShowError(false);
+    // } else {
+    //   setShowError(true); // If the username is not valid, show the error message.
+    // }
   }
 
-  function verifyPassword() {
-    // Check database if password is correct for the given username, if not show error.
-    // The password checking logic will differ based on the security level (mode).
-    if (mode === "Vulnerable") {
-      // Simple password check (against passwords in the database)
-      const userPasswordFromDB = "placeholder"; // [SQL injection vulnerability here!] TODO: Replace with actual password retrieval from the database for the given username.
-      if (enteredPassword === userPasswordFromDB) {
-        setIsLoggedIn(true); // If the password is correct, set logged-in state to true.
-      } else {
-        setShowError(true); // If the password is incorrect, show the error message.
-      }
-    } else {
-      // mode is "Secure":
+  // function verifyPassword() {
+  //   // Check database if password is correct for the given username, if not show error.
+  //   // The password checking logic will differ based on the security level (mode).
+  //   if (mode === "Vulnerable") {
+  //     // Simple password check (against passwords in the database)
+  //     const userPasswordFromDB = "placeholder"; // [SQL injection vulnerability here!] TODO: Replace with actual password retrieval from the database for the given username.
+  //     if (enteredPassword === userPasswordFromDB) {
+  //       setIsLoggedIn(true); // If the password is correct, set logged-in state to true.
+  //       setShowHashError(false);
+  //     } else {
+  //       setShowError(true); // If the password is incorrect, show the error message.
+  //     }
+  //   } else {
+  //     // mode is "Secure":
 
-      const salt = randomBytes(16).toString("hex"); // Generate a random salt
+  //     const salt = randomBytes(16).toString("hex"); // Generate a random salt
 
-      /* 
-      Hash and salt password and check against the stored hash in the database.
-      64: key length (length of the derived key in bytes).
-      N: Cost parameter (CPU/memory cost), higher number better but slower.
-      r: Block size parameter, affects memory usage and parallelization.
-      p: Parallelization parameter, affects how many parallel threads are used to compute the hash.
-      */
-      scrypt(
-        enteredPassword,
-        salt,
-        64,
-        { N: 16384, r: 8, p: 1 },
-        (err, derivedKey) => {
-          if (err) throw err;
-          const hashedPassword = derivedKey.toString("hex"); // Convert derived key to hexadecimal.
+  //     /*
+  //     Hash and salt password and check against the stored hash in the database.
+  //     64: key length (length of the derived key in bytes).
+  //     N: Cost parameter (CPU/memory cost), higher number better but slower.
+  //     r: Block size parameter, affects memory usage and parallelization.
+  //     p: Parallelization parameter, affects how many parallel threads are used to compute the hash.
+  //     */
+  //     try {
+  //       scrypt(
+  //         enteredPassword,
+  //         salt,
+  //         64,
+  //         { N: 16384, r: 8, p: 1 },
+  //         (err, derivedKey) => {
+  //           if (err) throw err;
+  //           const hashedPassword = derivedKey.toString("hex"); // Convert derived key to hexadecimal.
 
-          // Check the hashed password against the stored hash in the database for the given username.
-          const userPasswordHashFromDB = "placeholder"; // [SQL injection vulnerability here! FIX LATER!] TODO: Replace with actual password hash retrieval from the database for the given username.
-
-          if (hashedPassword === userPasswordHashFromDB) {
-            setIsLoggedIn(true); // If the password is correct, set logged-in state to true.
-          } else {
-            setShowError(true); // If the password is incorrect, show the error message.
-          }
-        },
-      );
-    }
-  }
+  //           // Check the hashed password against the stored hash in the database for the given username.
+  //           const userPasswordHashFromDB = "placeholder"; // [SQL injection vulnerability here! FIX LATER!] TODO: Replace with actual password hash retrieval from the database for the given username.
+  //           if (hashedPassword === userPasswordHashFromDB) {
+  //             setIsLoggedIn(true); // If the password is correct, set logged-in state to true.
+  //             setShowHashError(false);
+  //           } else {
+  //             setShowError(true); // If the password is incorrect, show the error message.
+  //           }
+  //         },
+  //       );
+  //     } catch (error) {
+  //       console.error("Error hashing password:", error);
+  //       setShowHashError(true); // Set the hash error state to true.
+  //     }
+  //   }
+  // }
 
   return (
     <div className="login-container">
@@ -109,6 +120,11 @@ function LoginForm({ setIsLoggedIn, mode }) {
         {showError && (
           <p className="error-message">
             Invalid username or password. Please try again.
+          </p>
+        )}
+        {showHashError && (
+          <p className="error-message">
+            Password processing error. Please try again.
           </p>
         )}
       </form>
