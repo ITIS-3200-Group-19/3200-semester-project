37350acf0da52632ae1f6abf7956826fce284391
alia27701 <aalimi1@charlotte.edu>
2026-04-27 13:13:05 -0400

    Changed query to show everything in backend

--- backend/server/routing/user.js
+++ backend/server/routing/user.js
@@ -9,7 +9,7 @@ const crypto = require("crypto");
 // Test to return the users of the SQL database
 router.get("/", async (req, res) => {
   try {
-    const [rows] = await database.query("SELECT id, username, mode FROM User");
+    const [rows] = await database.query("SELECT * FROM User");
     res.json({ success: true, count: rows.length, result: rows });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
